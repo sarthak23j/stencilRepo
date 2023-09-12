c89a444ce46c5a1ccb39a257592fb4cceecae936
Sarthak <sarthak23j@gmail.com>
2023-09-12 14:12:28 +0530

    add to cart works

--- src/components/product-text/product-text.tsx
+++ src/components/product-text/product-text.tsx
@@ -1,4 +1,5 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Listen, Prop, State } from '@stencil/core';
+
 
 @Component({
   tag: 'product-text',
@@ -7,16 +8,22 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class FirstComponent {
   @Prop() desc: string;
-
+  @State() added: boolean = false;
   @Prop() price: number;
 
+
+  @Listen('click')
+  handleClick(){
+    this.added = !this.added;
+  }
+  
   render() {
     return (
       <div class="product-text">
         <p class="product-desc">{this.desc} </p>
         <p class="product-price">
           Price : ${this.price}
-          <button class="buy-btn">Add to cart</button>
+          <button class="buy-btn">{this.added ? "Remove from cart" : "Add to cart"}</button>
         </p>
       </div>
     );
