97450309ffdee56525ac5ae827502064134d5d5b
Sarthak <sarthak23j@gmail.com>
2023-09-12 15:36:54 +0530

    cart now functions at a basic level

--- src/components/product-carousel/product-carousel.tsx
+++ src/components/product-carousel/product-carousel.tsx
@@ -1,31 +1,49 @@
-import { Component, h, Host, Listen } from "@stencil/core";
+import { Component, h, Host, Listen, Prop, State } from "@stencil/core";
 import Product from "./product";
-
-function dlcart(){
-
-  console.log("Download clicked!")
-  return;
-}
 @Component({
   tag: "product-carousel",
-    styleUrl: 'product-carousel.css',
-    shadow: true,
+  styleUrl: 'product-carousel.css',
+  shadow: true,
 })
 export class ProductCarousel {
+  
+  @State() cart: number[] = []
+
+  updateCart(i: number) {
+    let x = true;
+    for(let j = 0 ; j<=this.cart.length ; j++){
+      if (i === this.cart[j]){
+        this.cart = this.cart.filter((elem) => elem != i)
+        x = false;
+      }
+    }
+    if(x){
+      this.cart.push(i)
+    }
+
+    console.log(this.cart)
+  }
+  
+  dlcart(){
+    console.log("Download clicked!")
+    console.log(this.cart)
+    return;
+  }
 
   @Listen('cartChange')
   onCartChange(e) {
-    if(e.detail.added){
-
-    }
+    let i = e.detail[1];
+      this.updateCart(i);
+    
   }
+  
 
   render() {
     return (
       <Host>
         <header>
           <h1 class="page-title">Product Carousel Page</h1>
-          <button class="download" onClick={dlcart}>Download cart</button>
+          <button class="download" onClick={this.dlcart}>Download cart</button>
         </header>
         <div class="carousel">
           {Product.map((product) => (
