68b89771bc3a2cf11c414b962c87f1d732199306
Sarthak <sarthak23j@gmail.com>
2023-09-12 11:29:08 +0530

    varied desc lengths applied

--- src/components/product-carousel/product.ts
+++ src/components/product-carousel/product.ts
@@ -2,7 +2,7 @@ const Product = [
     {
       src: 'https://source.unsplash.com/random?blue',
       name: 'Product 1',
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae ad repellat quibusdam corporis ab ullam iste, mollitia perferendis quisquam sed possimus necessitatibus sapiente corrupti quas illo aspernatur nobis quae perspiciatis rem cupiditate quam blanditiis ducimus. Earum doloribus, dolorem quo at nisi soluta amet praesentium minima laboriosam eligendi dicta, suscipit, in quisquam reiciendis nam nesciunt repellendus accusantium deserunt. Deserunt ipsum voluptate nam vitae, illum et voluptatibus laudantium consectetur reprehenderit cumque reiciendis, unde earum aspernatur iste, quasi in tempore aliquam? Ad repellendus excepturi, eius alias laudantium dolore nihil voluptatum tenetur ex sint dolor perferendis veniam recusandae facere quos! Unde, omnis ducimus?",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consequatur quae doloremque totam fugit sapiente suscipit perspiciatis, aliquam eius quos hic voluptates! Facere, provident animi.",
       price: 10.00,
     },
     {
@@ -14,7 +14,7 @@ const Product = [
     {
       src: 'https://source.unsplash.com/random?green',
       name: 'Product 3',
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae ad repellat quibusdam corporis ab ullam iste, mollitia perferendis quisquam sed possimus necessitatibus sapiente corrupti quas illo aspernatur nobis quae perspiciatis rem cupiditate quam blanditiis ducimus. Earum doloribus, dolorem quo at nisi soluta amet praesentium minima laboriosam eligendi dicta, suscipit, in quisquam reiciendis nam nesciunt repellendus accusantium deserunt. Deserunt ipsum voluptate nam vitae, illum et voluptatibus laudantium consectetur reprehenderit cumque reiciendis, unde earum aspernatur iste, quasi in tempore aliquam? Ad repellendus excepturi, eius alias laudantium dolore nihil voluptatum tenetur ex sint dolor perferendis veniam recusandae facere quos! Unde, omnis ducimus?",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, hic? Qui, hic quisquam harum enim fuga laborum unde cum saepe? Laudantium aut ea blanditiis eius odit incidunt, ullam, distinctio minus quos quisquam ipsa voluptatibus similique, cupiditate quae asperiores accusantium consectetur repudiandae nisi qui pariatur doloremque laboriosam. Sit doloribus vel vero!",
       price: 9.99 ,
     },
   ];
