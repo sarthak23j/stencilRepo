4729b62cb0e32fae86f173f7b34d2aefbe146c49
Sarthak <sarthak23j@gmail.com>
2023-09-12 12:11:00 +0530

    added some styling

--- src/components/product-carousel/product-carousel.tsx
+++ src/components/product-carousel/product-carousel.tsx
@@ -1,89 +1,27 @@
-// import { Component, Prop, h } from '@stencil/core';
-
-// @Component({
-//   tag: 'product-carousel',
-//   styleUrl: 'product-carousel.css',
-//   shadow: true,
-// })
-// export class ProductCarousel {
-
-//   @Prop() products: any;
-  
-//   @Prop() data: any = {
-//     "products": [
-//       {
-//         "src": "abc",
-//         "name": "a",
-//         "desc": "aaa",
-//         "price": 10.00
-//       },
-//       {
-//         "src": "def",
-//         "name": "d",
-//         "desc": "ddd",
-//         "price": 9.99
-//       },
-//       {
-//         "src": "ghi",
-//         "name": "g",
-//         "desc": "ggg",
-//         "price": 20.50
-//       }
-//     ]
-//   }
-
-//   parseData() {
-//     const dataHere = JSON.parse(this.data);
-//     console.log(dataHere);
-//     this.products = dataHere.products.map((product) => {
-//       // return {
-//       //   src: product.src,
-//       //   name: product.name,
-//       //   desc: product.desc,
-//       //   price: product.price,
-//       // };
-
-//       return(
-//         <product-item
-//           src={product.src}
-//           name={product.name}
-//           desc={product.desc}
-//           price={product.price}
-//         >
-//         </product-item>
-//       )
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div class="carousel">
-//         {this.parseData()}
-//       </div>
-//     );
-//   }
-
-// }
-
-
-import { Component, h } from "@stencil/core";
+import { Component, h, Host } from "@stencil/core";
 import Product from "./product";
 @Component({
   tag: "product-carousel",
+    styleUrl: 'product-carousel.css',
+    shadow: true,
 })
 export class ProductCarousel {
   render() {
     return (
-      <div class="carousel">
-        {Product.map((product) => (
-          <product-item
-            src={product.src}
-            name={product.name}
-            desc={product.desc}
-            price={product.price}
-          ></product-item>
-        ))}
-      </div>
+      <Host>
+        <h1 class="page-title">Product Carousel Page</h1>
+
+        <div class="carousel">
+          {Product.map((product) => (
+            <product-item
+              src={product.src}
+              name={product.name}
+              desc={product.desc}
+              price={product.price}
+            ></product-item>
+          ))}
+        </div>
+      </Host>
     );
   }
 }
